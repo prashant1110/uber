3df2d6010f8ccf000af67afdf88591f8dd465388
Prashant Singh <prashant@codetailor.in>
2024-12-02 14:14:12 +0530

    complete logout feature

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,10 +1,17 @@
 const jwt = require("jsonwebtoken");
 const useModel = require("../models/user.model");
+const blackListModel = require("../models/blackListToken.model");
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization.split(" ")[1];
   
   if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlackListToken = await blackListModel.findOne({ token: token });
+
+  if (isBlackListToken) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 const { body } = require("express-validator");
-const authMiddleware=require("../middleware/auth.middleware")
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post(
   "/register",
@@ -29,6 +29,8 @@ router.post(
   userController.loginUser
 );
 
-router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
+router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
+
+router.get("/logout", authMiddleware.authUser, userController.logoutUser);
 
 module.exports = router;
