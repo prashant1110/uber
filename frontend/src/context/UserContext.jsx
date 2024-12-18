637fab4c4ce18647b003eddf65716322ef77dbcb
Prashant Singh <prashant@codetailor.in>
2024-12-18 16:50:26 +0530

    START: intialize tailwind and setup login and signup page

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+
+export const DataContext = createContext();
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullname: {
+      firstname: "",
+      lastname: "",
+    },
+  });
+
+  return (
+    <DataContext.Provider value={[user, setUser]}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export default UserContext;
